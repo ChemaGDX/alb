9861fb5002a8bc1e3e1ac0d9d0f06746d173a286
ChemaGDX <158061260+ChemaGDX@users.noreply.github.com>
2025-03-11 07:50:16 +0100

    Add files via upload

--- joy.json.js
+++ joy.json.js
@@ -14,7 +14,7 @@ let joy =
                     "orden": 1,
                     "nombre": "Disorder",
                     "duracion": "03:36",
-                    "letra": "",
+                    "letra": "I've been waiting for a guide to come and take me by the hand\nCould these sensations make me feel the pleasures of a normal man?\nThese sensations barely interest me for another day\nI've got the spirit, lose the feeling, take the shock away\n\nIt's getting faster, moving faster now, it's getting out of hand\nOn the tenth floor, down the back stairs, it's a no man's land\nLights are flashing, cars are crashing, getting frequent now\nI've got the spirit, lose the feeling, let it out somehow\n\nWhat means to you, what means to me, and we will meet again\nI'm watching you, I'm watching her, I'll take no pity from you friends\nMother I tried please believe me, I'm doing the best that I can\nI'm ashamed of the things I've been put through, I'm ashamed of the person I am",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -24,7 +24,7 @@ let joy =
                     "orden": 2,
                     "nombre": "Day of the Lords",
                     "duracion": "04:49",
-                    "letra": "",
+                    "letra": "Where will it end? Where will it end?\nWhere will it end? Where will it end?\n\nThis is the room, the start of it all\nNo portrait so fine, only sheets on the wall\nI've seen the nights filled with bloodsport and pain\nAnd the bodies obtained, the bodies obtained\n\nWhere will it end? Where will it end?\nWhere will it end? Where will it end?\n\nThese are your friends from childhood, through youth\nWho goaded you on, demanded more proof\nWithdrawal pain is hard, it can do you right in\nDistorted and thin, distorted and thin\n\nWhere will it end? Where will it end?\nWhere will it end? Where will it end?\n\nThis is the car at the edge of the road\nThere's nothing disturbed, all the windows are closed\nI guess you were right, when we talked in the heat\nThere's no room for the weak, no room for the weak",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -34,7 +34,7 @@ let joy =
                     "orden": 3,
                     "nombre": "Candidate",
                     "duracion": "03:05",
-                    "letra": "",
+                    "letra": "I campaigned for nothing, I worked hard for this\nI tried to get to you, you treat me like this\nI'm not a candidate for cancer or a substitute for your tears\nI'm not a candidate for a long lost hope that disappeared\n\nI tried to get to you, I tried to get to you\nI tried to get to you, I tried to get to you\n\nForced by the pressure, the territories marked\nNo longer the pleasure, oh, I've since lost the heart\nCorrupted from memory, no longer the power\nIt's creeping up slowly, that last fatal hour",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -44,7 +44,7 @@ let joy =
                     "orden": 4,
                     "nombre": "Insight",
                     "duracion": "04:29",
-                    "letra": "",
+                    "letra": "Guess your dreams always end\nThey don't rise up just descend\nBut I don't care anymore\nI've lost the will to want more\n\nI'm not afraid not at all\nI watch them all as they fall\nBut I remember when we were young\nWe were the ones they'd rely on\n\nBut now it's different, you see\nIt's the opposite, it's the opposite\nIt's the opposite, it's the opposite\n\nMother I tried please believe me\nI'm doing the best that I can\nI'm ashamed of the things I've been put through\nI'm ashamed of the person I am",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -53,8 +53,8 @@ let joy =
                     "id": 4,
                     "orden": 5,
                     "nombre": "New Dawn Fades",
-                    "duracion": "04:47",
-                    "letra": "",
+                    "duracion": "04:48",
+                    "letra": "A change of speed, a change of style\nA change of scene, with no regrets\nA chance to watch, admire the distance\nStill occupied, though you forget\n\nDifferent colors, different shades\nOver each mistakes were made\nI took the blame\nDirectionless, so plain to see\nA loaded gun won't set you free\nSo you say\n\nWe'll share a drink and step outside\nAn angry voice and one who cried\nWe'll give you everything and more\nThe strain's too much, can't take much more\n\nOh, I've walked on water, run through fire\nCan't seem to feel it anymore\nIt was me, waiting for me\nHoping for something more\nMe, seeing me this time, hoping for something else",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -64,7 +64,7 @@ let joy =
                     "orden": 6,
                     "nombre": "She's Lost Control",
                     "duracion": "03:57",
-                    "letra": "",
+                    "letra": "Confusion in her eyes that says it all\nShe's lost control\nAnd she's clinging to the nearest passerby\nShe's lost control\n\nAnd she gave away the secrets of her past\nAnd said I've lost control again\nAnd of a voice that told her when and where to act\nShe said I've lost control again\n\nAnd she turned around and took me by the hand and said\nI've lost control again\nAnd how I'll never know just why or understand\nShe said I've lost control again\n\nAnd she screamed out kicking on her side and said\nI've lost control again\nAnd seized up on the floor, I thought she'd die\nShe said I've lost control\n\nShe's lost control again\nShe's lost control again\nShe's lost control again\nShe's lost control",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -74,7 +74,7 @@ let joy =
                     "orden": 7,
                     "nombre": "Shadowplay",
                     "duracion": "03:55",
-                    "letra": "",
+                    "letra": "To the centre of the city in the night, waiting for you\nTo the centre of the city in the night, waiting for you\n\nI was moving through the silence without motion, waiting for you\nIn a room with a window in the corner I found truth\n\nIn the shadowplay, acting out your own death, knowing no more\nIn the shadowplay, acting out your own death, knowing no more\n\nTo the centre of the city in the night, waiting for you\nTo the centre of the city in the night, waiting for you\n\nI was moving through the silence without motion, waiting for you\nIn a room with a window in the corner I found truth\n\nIn the shadowplay, acting out your own death, knowing no more\nIn the shadowplay, acting out your own death, knowing no more",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -84,27 +84,7 @@ let joy =
                     "orden": 8,
                     "nombre": "Wilderness",
                     "duracion": "02:38",
-                    "letra": "",
-                    "letra2": "",
-                    "porcentaje": 0,
-                    "sentimiento": 0.0
-                },
-                {
-                    "id": 8,
-                    "orden": 9,
-                    "nombre": "Interzone",
-                    "duracion": "02:16",
-                    "letra": "",
-                    "letra2": "",
-                    "porcentaje": 0,
-                    "sentimiento": 0.0
-                },
-                {
-                    "id": 9,
-                    "orden": 10,
-                    "nombre": "I Remember Nothing",
-                    "duracion": "05:56",
-                    "letra": "",
+                    "letra": "In the wilderness, in the wilderness\nIn the wilderness, in the wilderness\n\nI walked through the city limits, someone talked me in\nI walked through the city limits, someone talked me in\n\nI walked through the city limits, someone talked me in\nI walked through the city limits, someone talked me in\n\nIn the wilderness, in the wilderness\nIn the wilderness, in the wilderness",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
@@ -120,7 +100,7 @@ let joy =
             "sentimiento": 0.0,
             "pistas": [
                 {
-                    "id": 0,
+                    "id": 8,
                     "orden": 1,
                     "nombre": "Atrocity Exhibition",
                     "duracion": "06:06",
@@ -130,7 +110,7 @@ let joy =
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 1,
+                    "id": 9,
                     "orden": 2,
                     "nombre": "Isolation",
                     "duracion": "02:53",
@@ -140,7 +120,7 @@ let joy =
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 2,
+                    "id": 10,
                     "orden": 3,
                     "nombre": "Passover",
                     "duracion": "04:46",
@@ -150,7 +130,7 @@ let joy =
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 3,
+                    "id": 11,
                     "orden": 4,
                     "nombre": "Colony",
                     "duracion": "03:55",
@@ -160,7 +140,7 @@ let joy =
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 4,
+                    "id": 12,
                     "orden": 5,
                     "nombre": "A Means to an End",
                     "duracion": "04:07",
@@ -170,17 +150,17 @@ let joy =
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 5,
+                    "id": 13,
                     "orden": 6,
                     "nombre": "Heart and Soul",
-                    "duracion": "05:52",
+                    "duracion": "05:51",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 6,
+                    "id": 14,
                     "orden": 7,
                     "nombre": "Twenty Four Hours",
                     "duracion": "04:26",
@@ -190,20 +170,20 @@ let joy =
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 7,
+                    "id": 15,
                     "orden": 8,
                     "nombre": "The Eternal",
-                    "duracion": "06:08",
+                    "duracion": "06:07",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 8,
+                    "id": 16,
                     "orden": 9,
                     "nombre": "Decades",
-                    "duracion": "06:11",
+                    "duracion": "06:10",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
@@ -214,86 +194,106 @@ let joy =
         {
             "id": 2,
             "artista": "Joy Division",
-            "titulo": "Still",
-            "anyo": 1981,
+            "titulo": "Substance",
+            "anyo": 1988,
             "porcentaje": 0,
             "sentimiento": 0.0,
             "pistas": [
                 {
-                    "id": 0,
+                    "id": 17,
                     "orden": 1,
-                    "nombre": "Exercise One",
-                    "duracion": "03:06",
+                    "nombre": "Warsaw",
+                    "duracion": "02:26",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 1,
+                    "id": 18,
                     "orden": 2,
-                    "nombre": "Ice Age",
-                    "duracion": "02:24",
+                    "nombre": "Leaders of Men",
+                    "duracion": "02:34",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 2,
+                    "id": 19,
                     "orden": 3,
-                    "nombre": "The Sound of Music",
-                    "duracion": "03:55",
+                    "nombre": "Digital",
+                    "duracion": "02:50",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 3,
+                    "id": 20,
                     "orden": 4,
-                    "nombre": "Glass",
-                    "duracion": "03:56",
+                    "nombre": "Autosuggestion",
+                    "duracion": "06:08",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 4,
+                    "id": 21,
                     "orden": 5,
-                    "nombre": "The Only Mistake",
-                    "duracion": "04:16",
+                    "nombre": "Transmission",
+                    "duracion": "03:36",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 5,
+                    "id": 22,
                     "orden": 6,
-                    "nombre": "Walked in Line",
-                    "duracion": "02:46",
+                    "nombre": "She's Lost Control",
+                    "duracion": "04:53",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 6,
+                    "id": 23,
                     "orden": 7,
-                    "nombre": "The Kill",
-                    "duracion": "02:15",
+                    "nombre": "Incubation",
+                    "duracion": "02:52",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
                     "sentimiento": 0.0
                 },
                 {
-                    "id": 7,
+                    "id": 24,
                     "orden": 8,
-                    "nombre": "Something Must Break",
-                    "duracion": "02:53",
+                    "nombre": "Dead Souls",
+                    "duracion": "04:56",
+                    "letra": "",
+                    "letra2": "",
+                    "porcentaje": 0,
+                    "sentimiento": 0.0
+                },
+                {
+                    "id": 25,
+                    "orden": 9,
+                    "nombre": "Atmosphere",
+                    "duracion": "04:10",
+                    "letra": "",
+                    "letra2": "",
+                    "porcentaje": 0,
+                    "sentimiento": 0.0
+                },
+                {
+                    "id": 26,
+                    "orden": 10,
+                    "nombre": "Love Will Tear Us Apart",
+                    "duracion": "03:25",
                     "letra": "",
                     "letra2": "",
                     "porcentaje": 0,
